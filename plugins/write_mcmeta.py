c192e75d93c8770fc35ce37a2311a4f88a1b91b3
runcows <alexparbarnes@gmail.com>
2026-06-16 19:52:55 -0400

    Bump version for 26.2

--- plugins/write_mcmeta.py
+++ plugins/write_mcmeta.py
@@ -1,11 +1,14 @@
 from beet import Context
 
 def beet_default(ctx: Context):
+  ctx.data.min_format = 101
+  ctx.data.max_format = 107
+  
   for pack in ctx.packs:
     pack.mcmeta.data["id"] = ctx.project_id
     
     pack.description = [
-      f"{ctx.project_name} v{ctx.project_version}",
+      f"{ctx.project_name} v{ctx.project_version}\n",
       {
         "text": ctx.project_author,
         "color": "#da3eb3"
