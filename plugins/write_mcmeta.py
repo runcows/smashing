4058962d18481b5574955c1d2f35bd73671591ac
runcows <alexparbarnes@gmail.com>
2026-09-15 21:08:21 -0400

    Update to 26.3

--- plugins/write_mcmeta.py
+++ plugins/write_mcmeta.py
@@ -1,8 +1,8 @@
 from beet import Context
 
 def beet_default(ctx: Context):
-  ctx.data.min_format = 101
-  ctx.data.max_format = 107
+  ctx.data.min_format = 121
+  ctx.data.max_format = 121
   
   for pack in ctx.packs:
     pack.mcmeta.data["id"] = ctx.project_id
